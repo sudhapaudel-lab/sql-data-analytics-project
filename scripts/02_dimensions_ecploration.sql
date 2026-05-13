2f6eb69d76d38511a2d9bc2fb143d8a848b7ed63
Sudha Paudel <136981104+sudhapaudel-lab@users.noreply.github.com>
2026-05-13 09:00:18 +0545

    Delete scripts directory

--- scripts/02_dimensions_ecploration.sql
+++
@@ -1,8 +0,0 @@
---Exploring a dimensions
--- Explore All countries our customers come from
-SELECT DISTINCT country 
-FROM gold.dim_customers
-
-SELECT DISTINCT category, sub_category, product_name
-FROM gold.dim_products
-ORDER BY 1,2,3
